c7d7d890d854bdc6083b95595b1577f0747ca85c
Viktor Karasev <viktorkarasev27@gmail.com>
2024-03-06 21:31:13 +0300

    [feat] Optimized Channel (used semaphore instead spin lock)

--- ThreadPoolHomework/ThreadPoolHomework.CustomThreadPool/Internal/Channel.cs
+++ ThreadPoolHomework/ThreadPoolHomework.CustomThreadPool/Internal/Channel.cs
@@ -4,37 +4,34 @@ namespace ThreadPoolHomework.CustomThreadPool.Internal;
 
 internal class Channel<T>
 {
-    private const int ReadSpinTimeout = 10;
     private readonly ConcurrentQueue<T> _queue = new();
-    private readonly int _spinTimeout;
+    private readonly Semaphore _semaphore = new(0, 1);
+    private readonly int _waitTimeout;
 
-    public Channel(int spinTimeout)
+    public Channel(int waitTimeout)
     {
-        _spinTimeout = spinTimeout;
+        _waitTimeout = waitTimeout;
     }
 
     public void Write(T data)
     {
         _queue.Enqueue(data);
+        _semaphore.Release();
     }
 
     public T Read(CancellationToken token)
     {
-        var spinWait = new TwoStepSpinWait(ReadSpinTimeout);
         while (true)
         {
             token.ThrowIfCancellationRequested();
-            if (_queue.IsEmpty)
-            {
-                spinWait.SpinOnce();
-            }
-            else
+            if (_semaphore.WaitOne(_waitTimeout))
             {
                 var dequeued = _queue.TryDequeue(out var data);
                 if (dequeued && data != null)
                 {
                     return data;
                 }
+                throw new Exception("Read from empty channel");
             }
         }
     }
