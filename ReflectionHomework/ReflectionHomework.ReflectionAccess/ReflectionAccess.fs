09b08e45329cba4f3dbb43c10a5b268e78ebea21
Viktor Karasev <viktorkarasev27@gmail.com>
2024-03-07 18:10:09 +0300

    [feat] Added expression access, refactored tests

--- ReflectionHomework/ReflectionHomework.ReflectionAccess/ReflectionAccess.fs
+++ ReflectionHomework/ReflectionHomework.ReflectionAccess/ReflectionAccess.fs
@@ -1,6 +1,7 @@
 namespace ReflectionHomework.ReflectionAccess
 
 open System
+open System.Linq.Expressions
 
 module ReflectionAccess =
 
@@ -25,6 +26,23 @@ module ReflectionAccess =
         |> Option.map Ok
         |> Option.defaultValue (Error $"'{name}' is not a field or a property")
 
+    let private createExpressionValueByName name bindingFlags (e: Expression) =
+
+        let getField () =
+            e.Type.GetField(name, bindingFlags)
+            |> Option.ofObj
+            |> Option.map (fun field -> Expression.Field(e, field) :> Expression)
+
+        let getProperty () =
+            e.Type.GetProperty(name, bindingFlags)
+            |> Option.ofObj
+            |> Option.map (fun property -> Expression.Property(e, property) :> Expression)
+
+        getField ()
+        |> Option.orElseWith getProperty
+        |> Option.map Ok
+        |> Option.defaultValue (Error $"'{name}' is not a field or a property for type {e.Type}")
+
     let private getValueByIndex (t: Type) index (bindingFlags: Reflection.BindingFlags) obj =
 
         let getItem () =
@@ -36,10 +54,26 @@ module ReflectionAccess =
         |> Option.map Ok
         |> Option.defaultValue (Error $"type '{t}' has not []")
 
+    let private createExpressionValueByIndex index (bindingFlags: Reflection.BindingFlags) (e: Expression) =
+
+        let getItem () =
+            e.Type.GetMethod("Get", bindingFlags)
+            |> Option.ofObj
+
+        getItem ()
+        |> Option.map (fun getMethod -> Expression.Call(e, getMethod, Expression.Constant(index)) :> Expression)
+        |> Option.map Ok
+        |> Option.defaultValue (Error $"type '{e.Type}' has not []")
+
     let private getValueByAccessor (t: Type) accessor bindingFlags =
         match accessor with
         | ByName name -> getValueByName t name bindingFlags
         | ByIndex index -> getValueByIndex t index bindingFlags
+
+    let private createExpressionValueByAccessor accessor bindingFlags =
+        match accessor with
+        | ByName name -> createExpressionValueByName name bindingFlags
+        | ByIndex index -> createExpressionValueByIndex index bindingFlags
 
     let private getValue accessors bindingFlags obj =
         accessors
@@ -48,6 +82,14 @@ module ReflectionAccess =
             | Ok obj -> getValueByAccessor (obj.GetType()) accessor bindingFlags obj
             | x -> x
         ) (Ok obj)
+
+    let private createExpression accessors bindingFlags expr =
+        accessors
+        |> List.fold (fun currentExpression accessor ->
+            match currentExpression with
+            | Ok expr -> createExpressionValueByAccessor accessor bindingFlags expr
+            | x -> x
+        ) (Ok expr)
 
     open FParsec.Primitives
     open FParsec
@@ -74,6 +116,15 @@ module ReflectionAccess =
         | Success (_, _, endPosition) -> Result.Error $"Can not parse {str[int endPosition.Index..]}"
         | Failure (errAsString, _, _) -> Result.Error errAsString
 
-    let reflectionAccess bindingFlags accessString  obj =
+    let reflectionAccess bindingFlags accessString obj: Result<'r, string> =
         parseString accessString
         |> Result.bind (fun x -> getValue x bindingFlags obj)
+        |> Result.map (fun r -> r :?> 'r)
+
+    let expressionAccess bindingFlags accessString (obj: 't): Result<'r, string> =
+        let param = Expression.Parameter(typeof<'t>)
+
+        parseString accessString
+        |> Result.bind (fun x -> createExpression x bindingFlags param)
+        |> Result.map (fun e -> Expression.Lambda<Func<'t, 'r>>(e, param).Compile())
+        |> Result.map (fun f -> f.Invoke obj)
--- ReflectionHomework/ReflectionHomework.Tests/ReflectionAccessTests.fs
+++ ReflectionHomework/ReflectionHomework.Tests/ReflectionAccessTests.fs
@@ -1,6 +1,6 @@
 module ReflectionHomework.Tests
 
-open NUnit.Framework
+open Expecto
 
 let allBindingFlags () =
     let (|||) = Microsoft.FSharp.Core.Operators.(|||)
@@ -12,15 +12,21 @@ let allBindingFlags () =
 let reflectionAccess accessString obj =
     ReflectionHomework.ReflectionAccess.ReflectionAccess.reflectionAccess (allBindingFlags ()) accessString obj
 
+let expressionAccess accessString obj =
+    ReflectionHomework.ReflectionAccess.ReflectionAccess.expressionAccess (allBindingFlags ()) accessString obj
+
+let accessors = [ reflectionAccess; expressionAccess ]
+
+
 let assertError result =
     match result with
-    | Ok _ -> Assert.Fail()
-    | Error _ -> Assert.Pass()
+    | Ok _ -> failwith "Expected Error, but got Ok"
+    | Error _ -> ()
 
 let assertOk value result =
     match result with
-    | Ok v -> Assert.AreEqual(value, v)
-    | Error _ -> Assert.Fail()
+    | Ok v when v = value -> ()
+    | x -> failwith $"Expected Ok {value}, but got %A{x}"
 
 let mkSet (lst: seq<_>) = System.Collections.Generic.SortedSet<_>(lst)
 let mkArray = Seq.toArray
@@ -32,80 +38,89 @@ type ClassWithPublicField =
     val PublicField: int
     new (x) = { PublicField = x }
 
-[<Test>]
-let ``incorrect access string`` () =
-    mkSet [1; 2]
-    |> reflectionAccess "abc"
-    |> assertError
+// We need wrapper because generic inference for lambda cause "this construct causes code to be less generic"
+type IAccessorWrapper =
+   abstract member Access: string -> 'a -> Result<'r, string>
 
-[<Test>]
-let ``public property access`` () =
-    mkSet [1; 2]
-    |> reflectionAccess ".Count"
-    |> assertOk 2
 
-[<Test>]
-let ``incorrect public property access`` () =
-    mkSet [1; 2]
-    |> reflectionAccess ".Countt"
-    |> assertError
+let createTests testListName (accessor: IAccessorWrapper) =
 
-[<Test>]
-let ``public field access`` () =
-    ClassWithPublicField(1)
-    |> reflectionAccess ".PublicField"
-    |> assertOk 1
+    testList testListName <| [
+        testCase "incorrect access string" <| fun () ->
+            mkSet [1; 2]
+            |> accessor.Access "abc"
+            |> assertError
 
-[<Test>]
-let ``incorrect public field access`` () =
-    ClassWithPublicField(1)
-    |> reflectionAccess ".PubliccField"
-    |> assertError
+        testCase "public property access" <| fun () ->
+            mkSet [1; 2]
+            |> accessor.Access ".Count"
+            |> assertOk 2
 
-[<Test>]
-let ``private property access`` () =
-    mkSet [1; 2]
-    |> reflectionAccess ".MaxInternal"
-    |> assertOk 2
+        testCase "incorrect public property access" <| fun () ->
+            mkSet [1; 2]
+            |> accessor.Access ".Countt"
+            |> assertError
 
-[<Test>]
-let ``private field access`` () =
-    mkDict [(0, 1)]
-    |> reflectionAccess "._count"
-    |> assertOk 1
+        testCase "public field access" <| fun () ->
+            ClassWithPublicField(1)
+            |> accessor.Access ".PublicField"
+            |> assertOk 1
 
-[<Test>]
-let ``incorrect private property access`` () =
-    mkSet [1; 2]
-    |> reflectionAccess ".MaxInternall"
-    |> assertError
+        testCase "incorrect public field access" <| fun () ->
+            ClassWithPublicField(1)
+            |> accessor.Access ".PubliccField"
+            |> assertError
 
-[<Test>]
-let ``incorrect private field access`` () =
-    mkDict [(0, 1)]
-    |> reflectionAccess "_ccount"
-    |> assertError
+        testCase "private property access" <| fun () ->
+            mkSet [1; 2]
+            |> accessor.Access ".MaxInternal"
+            |> assertOk 2
 
-[<Test>]
-let ``index access`` () =
-    mkArray [1; 2]
-    |> reflectionAccess "[0]"
-    |> assertOk 1
+        testCase "private field access" <| fun () ->
+            mkDict [(0, 1)]
+            |> accessor.Access "._count"
+            |> assertOk 1
 
-[<Test>]
-let ``incorrect index access`` () =
-    mkArray [1; 2]
-    |> reflectionAccess "0]"
-    |> assertError
+        testCase "incorrect private property access" <| fun () ->
+            mkSet [1; 2]
+            |> accessor.Access ".MaxInternall"
+            |> assertError
 
-[<Test>]
-let ``multiple index access`` () =
-    mkArray [ mkArray [0] ]
-    |> reflectionAccess "[0][0]"
-    |> assertOk 0
+        testCase "incorrect private field access" <| fun () ->
+            mkDict [(0, 1)]
+            |> accessor.Access "_ccount"
+            |> assertError
 
-[<Test>]
-let ``mixed name/index public/private access`` () =
-    mkDict [(1, mkArray [0])]
-    |> reflectionAccess "._entries[0].value[0]"
-    |> assertOk 0
+        testCase "index access" <| fun () ->
+            mkArray [1; 2]
+            |> accessor.Access "[0]"
+            |> assertOk 1
+
+        testCase "incorrect index access" <| fun () ->
+            mkArray [1; 2]
+            |> accessor.Access "0]"
+            |> assertError
+
+        testCase "multiple index access" <| fun () ->
+            mkArray [ mkArray [0] ]
+            |> accessor.Access "[0][0]"
+            |> assertOk 0
+
+        testCase "mixed name/index public/private access" <| fun () ->
+            mkDict [(1, mkArray [0])]
+            |> accessor.Access "._entries[0].value[0]"
+            |> assertOk 0
+    ]
+
+[<EntryPoint>]
+let main _ =
+    runTestsWithCLIArgs [] [||] <| testList "All tests" [
+        createTests "Reflection" {
+            new IAccessorWrapper with
+                member this.Access name obj = reflectionAccess name obj
+        }
+        createTests "Expression" {
+            new IAccessorWrapper with
+                member this.Access name obj = expressionAccess name obj
+        }
+    ]
